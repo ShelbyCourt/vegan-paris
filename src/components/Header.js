4c12cf33436422ac90e292960527e88dce94d82d
ShelbyCourt <shelby.b.courtney@gmail.com>
2020-05-22 13:48:24 -0700

    Header - JSON

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header(props) {
+    return (
+        <header>
+            <h1> Allons-y! </h1>
+            <h2>Les restaurants végans de Paris</h2>
+        </header>
+    )
+}
+
+export default Header
